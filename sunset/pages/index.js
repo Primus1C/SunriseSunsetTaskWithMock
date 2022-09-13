15c408082447a73555c9dc272812a7ff923ac07b
Artur Rakhimov <kharkiv1c@gmail.com>
2022-09-13 17:19:27 +0200

    develop

--- sunset/pages/index.js
+++ sunset/pages/index.js
@@ -1,13 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Sunset from "../components/Sunset";
 
 export default function Home() {
   return (
-
-    // ! тут вызов компонента
-    <Sunset/>
-
-
+    <Sunset />
   )
 }
